bf37441f88e82bc2441c04bb812aacb5936c6291
Nalankumar <thirunalankumar@gmail.com>
2025-05-14 23:01:23 +0530

    shift accepting operation developed

--- backend/src/main/java/com/code_red/phc_attendance_system/controllers/ShiftController.java
+++ backend/src/main/java/com/code_red/phc_attendance_system/controllers/ShiftController.java
@@ -3,10 +3,16 @@ package com.code_red.phc_attendance_system.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.code_red.phc_attendance_system.dto.ShiftStatusDTO;
 import com.code_red.phc_attendance_system.entities.Shift;
 import com.code_red.phc_attendance_system.services.ShiftService;
 
@@ -18,7 +24,13 @@ public class ShiftController {
 	private ShiftService shiftService;
 
 	@GetMapping("/pending")
-	public List<Shift> getPendingShifts() {
-		return shiftService.getPendingShifts();
+	public ResponseEntity<List<Shift>> getPendingShifts() {
+		return new ResponseEntity<>(shiftService.getPendingShifts(), HttpStatus.ACCEPTED);
+	}
+	
+	@PutMapping("/{id}/status")
+	public ResponseEntity<Boolean> updateShift(@RequestBody ShiftStatusDTO shiftDTO, @PathVariable Long id) {
+		return shiftService.updateShift(shiftDTO.getStatus(), HttpStatus.OK);
+		
 	}
 }
---
+++ backend/src/main/java/com/code_red/phc_attendance_system/dto/ShiftStatusDTO.java
@@ -0,0 +1,14 @@
+package com.code_red.phc_attendance_system.dto;
+
+import com.code_red.phc_attendance_system.enums.ShiftStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShiftStatusDTO {
+	private String status;
+}
